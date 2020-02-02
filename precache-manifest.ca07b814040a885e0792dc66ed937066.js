854f674ccff400b23a969828004dcbe3b6eed20c
Simon Sandbom <Simon.sandbom@gmail.com>
2020-02-02 22:41:28 +0100

    Updates

---
+++ precache-manifest.ca07b814040a885e0792dc66ed937066.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648cacaed43b414cfe05ad28582e3c25",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "95ee7c5a971d56024eb8",
+    "url": "/portfolio2/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "e24c1a12d4b48dc570d6",
+    "url": "/portfolio2/static/css/main.95fe478e.chunk.css"
+  },
+  {
+    "revision": "95ee7c5a971d56024eb8",
+    "url": "/portfolio2/static/js/2.39f79ad2.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/portfolio2/static/js/2.39f79ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24c1a12d4b48dc570d6",
+    "url": "/portfolio2/static/js/main.52d7b545.chunk.js"
+  },
+  {
+    "revision": "8277b41483b27a5888e4",
+    "url": "/portfolio2/static/js/runtime-main.e1f2a71c.js"
+  }
+]);
